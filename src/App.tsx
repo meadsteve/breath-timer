40e4fa171842803a39d08cb92bc93e009c56b6ee
steve <steve@meadsteve.dev>
2022-02-16 15:18:29 +0100

    lock the wait state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import BreathTimer from "./BreathTimer";
 
 function App() {
+  useEffect(() => {
+    if ('wakeLock' in navigator) {
+      console.log("Locking the wake state");
+      // @ts-ignore
+      const wakeLock = navigator.wakeLock.request().then(() => console.log("Locked"));
+      return () => wakeLock.release().then("Unlocked");
+    }
+  });
   return (
     <div className="App">
         <BreathTimer in_time={5} in_hold={1} out_time={5} out_hold={1}/>
