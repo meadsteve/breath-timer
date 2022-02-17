d30840bda99ef47accf3c13942946ee1d545330b
Sarah Mottram <67631502+Smelbows@users.noreply.github.com>
2022-02-17 14:02:47 +0100

    css and a button (#2)
    * background image
    
    * styling and also a stop button in breathtimerconfigurer, with it's own styling

--- src/BreathTimerConfigerer.tsx
+++ src/BreathTimerConfigerer.tsx
@@ -1,7 +1,6 @@
-import {FormEvent, useState} from "react";
-import BreathTimer, {props as breathtimeProps} from "./BreathTimer"
-import "./BreathTimerConfigerer.css"
-
+import { FormEvent, useState } from 'react';
+import BreathTimer, { props as breathtimeProps } from './BreathTimer';
+import './BreathTimerConfigerer.css';
 
 export default function BreathTimerConfigerer() {
   const [values, setValues] = useState<breathtimeProps | undefined>(undefined);
@@ -16,34 +15,51 @@ export default function BreathTimerConfigerer() {
       // @ts-ignore
       in_hold: parseFloat(event.target.in_hold.value),
       // @ts-ignore
-      out_hold: parseFloat(event.target.out_hold.value)
-    })
+      out_hold: parseFloat(event.target.out_hold.value),
+    });
     event.preventDefault();
   }
 
+  const onReset = () => {
+    setValues(undefined);
+  };
+
   if (!values) {
-    return <form onSubmit={handleSubmit} className="pretty_form">
-      <label>
-        Breath in:
-        <input defaultValue={5} name="in_time" type="number"/>
-      </label>
-      <label>
-        Hold:
-        <input defaultValue={1} name="in_hold" type="number"/>
-      </label>
-      <label>
-        Breath out:
-        <input defaultValue={5} name="out_time" type="number"/>
-      </label>
-      <label>
-        Hold:
-        <input defaultValue={1} name="out_hold" type="number"/>
-      </label>
-      <input type="submit" value="Start"/>
-    </form>;
+    return (
+      <form onSubmit={handleSubmit} className="pretty_form">
+        <label>
+          Breath in:
+          <input defaultValue={5} name="in_time" type="number" />
+        </label>
+        <label>
+          Hold:
+          <input defaultValue={1} name="in_hold" type="number" />
+        </label>
+        <label>
+          Breath out:
+          <input defaultValue={5} name="out_time" type="number" />
+        </label>
+        <label>
+          Hold:
+          <input defaultValue={1} name="out_hold" type="number" />
+        </label>
+        <input type="submit" value="Start" />
+      </form>
+    );
   } else {
     console.log(values);
-    return <BreathTimer in_hold={values.in_hold} out_hold={values.out_hold} in_time={values.in_time}
-                        out_time={values.out_time}/>
+    return (
+      <>
+        <BreathTimer
+          in_hold={values.in_hold}
+          out_hold={values.out_hold}
+          in_time={values.in_time}
+          out_time={values.out_time}
+        />
+        <button className="reset-button" onClick={onReset}>
+          Stop
+        </button>
+      </>
+    );
   }
 }
