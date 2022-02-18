d8cd0ef743c54c0c4eff25d285ecb2f4d62530c4
steve <steve@meadsteve.dev>
2022-02-18 19:35:37 +0100

    add a little colour gradient

--- src/BreathTimer.tsx
+++ src/BreathTimer.tsx
@@ -1,78 +1,100 @@
-import {useEffect, useState} from "react";
-import "./BreathTimer.css";
+import { useEffect, useState } from 'react';
+import './BreathTimer.css';
 
 export type props = {
-  in_time: number,
-  in_hold:number,
-  out_time: number,
-  out_hold: number
+  in_time: number;
+  in_hold: number;
+  out_time: number;
+  out_hold: number;
 };
 
-type BreathState = "in" | "in_hold" | "out" | "out_hold";
+type BreathState = 'in' | 'in_hold' | 'out' | 'out_hold';
 type StateConfig = {
-  [key in BreathState]: { duration: number; next_state: BreathState; };
+  [key in BreathState]: { duration: number; next_state: BreathState };
 };
 
-function BreathText({state, children}: {state: BreathState, children?: JSX.Element | JSX.Element[]}) {
-  let heading = "";
+function BreathText({
+  state,
+  children,
+}: {
+  state: BreathState;
+  children?: JSX.Element | JSX.Element[];
+}) {
+  let heading = '';
   switch (state) {
-    case "in":
-      heading = "Breath In";
+    case 'in':
+      heading = 'Breath In';
       break;
-    case "out":
-      heading = "Breath Out";
+    case 'out':
+      heading = 'Breath Out';
       break;
-    case "in_hold":
-    case "out_hold":
-      heading = "Hold";
+    case 'in_hold':
+    case 'out_hold':
+      heading = 'Hold';
       break;
   }
-  return <div className="breath-text">
-    <h2>{heading}</h2>
-    {children}
-  </div>;
+  return (
+    <div className="breath-text">
+      <h2>{heading}</h2>
+      {children}
+    </div>
+  );
 }
 
-function circleRadiusRatio(runningFor: number, states: StateConfig, state: BreathState) {
+function circleRadiusRatio(
+  runningFor: number,
+  states: StateConfig,
+  state: BreathState
+) {
   switch (state) {
-    case "in":
-      return 0.4 + 0.6 * runningFor / states[state].duration;
-    case "out":
-      return 1 - 0.6 * runningFor / states[state].duration;
-    case "in_hold":
+    case 'in':
+      return 0.4 + (0.6 * runningFor) / states[state].duration;
+    case 'out':
+      return 1 - (0.6 * runningFor) / states[state].duration;
+    case 'in_hold':
       return 1;
-    case "out_hold":
+    case 'out_hold':
       return 0.4;
   }
 }
 
-function circleColour(runningFor: number, states: StateConfig, state: BreathState) {
+function circleColour(
+  runningFor: number,
+  states: StateConfig,
+  state: BreathState
+) {
   const ratio = runningFor / states[state].duration;
   let colour_one = 200 - 130 * ratio;
   let colour_two = 70 + 130 * ratio;
   switch (state) {
-    case "in":
-      return `rgb(${colour_one}, 85, ${colour_two})`
-    case "in_hold":
-      return "rgb(70, 85, 200)"
-    case "out":
-      return `rgb(${colour_two}, 85, ${colour_one})`
-    case "out_hold":
-      return "rgb(200, 85, 70)"
+    case 'in':
+      return `rgb(${colour_one}, 85, ${colour_two})`;
+    case 'in_hold':
+      return 'rgb(70, 85, 200)';
+    case 'out':
+      return `rgb(${colour_two}, 85, ${colour_one})`;
+    case 'out_hold':
+      return 'rgb(200, 85, 70)';
   }
 }
 
-export default function BreathTimer({in_time, out_time, in_hold, out_hold}: props) {
+export default function BreathTimer({
+  in_time,
+  out_time,
+  in_hold,
+  out_hold,
+}: props) {
   const states: StateConfig = {
-    in: {duration: in_time, next_state: "in_hold"},
-    in_hold: {duration: in_hold, next_state: "out"},
-    out: {duration: out_time, next_state: "out_hold"},
-    out_hold: {duration: out_hold, next_state: "in"}
+    in: { duration: in_time, next_state: 'in_hold' },
+    in_hold: { duration: in_hold, next_state: 'out' },
+    out: { duration: out_time, next_state: 'out_hold' },
+    out_hold: { duration: out_hold, next_state: 'in' },
   };
 
   const [startedAt, setStartedAt] = useState(Date.now());
   const [runningFor, setRunningFor] = useState(0);
-  const [currentBreathState, setCurrentBreathState] = useState<BreathState>("in");
+  const [currentBreathState, setCurrentBreathState] =
+    useState<BreathState>('in');
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -90,13 +112,25 @@ export default function BreathTimer({in_time, out_time, in_hold, out_hold}: prop
     return () => clearInterval(interval);
   });
 
-  const radius = `${circleRadiusRatio(runningFor, states, currentBreathState) * 80}vmin`;
-  const background = circleColour(runningFor, states, currentBreathState);
+  const radius = `${
+    circleRadiusRatio(runningFor, states, currentBreathState) * 80
+  }vmin`;
+  const mainColour = circleColour(runningFor, states, currentBreathState);
+  const background = `radial-gradient(rgb(240, 240, 255), ${mainColour} 90%)`;
   const timeRemaining = states[currentBreathState].duration - runningFor;
 
-  return <div className="blue-circle" style={{width: radius, height: radius, background: background}}>
-    <BreathText state={currentBreathState}>
-      {states[currentBreathState].duration >= 2 ? <span className="time-remaining">{timeRemaining.toFixed(0)}</span> : <></>}
-    </BreathText>
-  </div>
+  return (
+    <div
+      className="blue-circle"
+      style={{ width: radius, height: radius, backgroundImage: background }}
+    >
+      <BreathText state={currentBreathState}>
+        {states[currentBreathState].duration >= 2 ? (
+          <span className="time-remaining">{timeRemaining.toFixed(0)}</span>
+        ) : (
+          <></>
+        )}
+      </BreathText>
+    </div>
+  );
 }
